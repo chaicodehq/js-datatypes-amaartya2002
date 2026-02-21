25f506eee21c9e1000cb87fc670979ca936e2383
Amartya <amartya.kumar2001@gmail.com>
2026-02-21 17:42:12 +0530

    Completed upi-transactions

--- src/10-upi-transaction-log.js
+++ src/10-upi-transaction-log.js
@@ -1,3 +1,5 @@
+import { use } from "react"
+
 /**
  * 💸 UPI Transaction Log Analyzer
  *
@@ -48,4 +50,99 @@
  */
 export function analyzeUPITransactions(transactions) {
   // Your code here
+  if (!Array.isArray(transactions) || transactions.length === 0) {
+    return null
+  }
+
+  const cat = ["credit", "debit"]
+  const filteredTrx = transactions.filter((trx) => trx.amount > 0 && cat.includes(trx.type))
+  //console.log(transactions);
+  //console.log(filteredTrx);
+
+  if (filteredTrx.length === 0) return null
+
+  const transactionCount = filteredTrx.length
+
+  const paise = filteredTrx.reduce((acc, trx) => {
+
+    const typeOfTrx = trx.type === "credit" ? "credit" : "debit"
+
+    acc[typeOfTrx] += trx.amount
+
+    acc["sum"] += trx.amount
+
+    return acc
+
+  }, {
+    credit: 0,
+    debit: 0,
+    sum: 0
+  })
+
+  // console.log(paise);
+
+  const totalCredit = paise["credit"]
+  const totalDebit = paise["debit"]
+
+  const netBalance = totalCredit - totalDebit
+
+  const avgTransaction = Math.round((paise["sum"] / transactionCount))
+
+
+
+
+  const categoryBreakdown = filteredTrx.reduce((acc, trx) => {
+
+    acc[trx.category] = (acc[trx.category] || 0) + trx["amount"]
+
+    return acc
+
+  }, {})
+
+
+
+  const freq = filteredTrx.reduce((acc, trx) => {
+    const to = trx["to"]
+
+    acc["count"][to] = (acc["count"][to] || 0) + 1
+
+    if (acc.count[to] > acc["maxCount"]) {
+      acc.maxCount = acc.count[to]
+      acc.freqContact = to
+    }
+
+    return acc
+
+  }, {
+    count: {},
+    maxCount: 0,
+    freqContact: null
+  })
+
+  const frequentContact = freq.freqContact
+
+  const highestTransaction = filteredTrx.reduce((acc, trx) => {
+    return acc.amount > trx.amount ? acc : trx
+  })
+
+  //console.log(highestTransaction);
+
+  const allAbove100 = filteredTrx.every((trx) => trx.amount > 100)
+
+  const hasLargeTransaction = filteredTrx.some((trx) => trx.amount >= 5000)
+
+
+  return {
+    totalCredit,
+    totalDebit,
+    netBalance,
+    transactionCount,
+    avgTransaction,
+    highestTransaction,
+    categoryBreakdown,
+    frequentContact,
+    allAbove100,
+    hasLargeTransaction
+  }
+
 }
